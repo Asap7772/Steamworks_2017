1390186f471bb0b69975e6b8e8a42823126bab84
Anikait Singh <singh.anikait@gmail.com>
2017-03-30 09:10:17 -0400

    Added print statements for debugging

--- src/org/usfirst/frc/team1923/robot/commands/drive/DriveStraightWithGyroCommand.java
+++ src/org/usfirst/frc/team1923/robot/commands/drive/DriveStraightWithGyroCommand.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1923.robot.commands.drive;
 
 import org.usfirst.frc.team1923.robot.Robot;
+import org.usfirst.frc.team1923.robot.RobotMap;
 
 import com.ctre.CANTalon.TalonControlMode;
 import com.ctre.PigeonImu;
@@ -15,6 +16,7 @@ public class DriveStraightWithGyroCommand extends Command {
     private double head;
 
     private final double P_CONST = 0.070;
+    private final double CONTROLLER_BIAS = 0;
     private final double TOLERANCE = 0.100;
 
     public DriveStraightWithGyroCommand(double distance) {
@@ -23,7 +25,8 @@ public class DriveStraightWithGyroCommand extends Command {
         this.setTimeout(Math.abs(distance) * 0.05 + 2);
         this.gyro = Robot.driveSubSys.getImu();
         // initial heading
-        this.head = this.gyro.GetFusedHeading(new FusionStatus()) % 360;
+        this.head = 0.0;
+        this.gyro.SetFusedHeading(head);
     }
 
     @Override
@@ -31,12 +34,12 @@ public class DriveStraightWithGyroCommand extends Command {
 
     }
 
-    /*
-     * The P-Only Algorithm The P-Only controller computes a CO action every
-     * loop sample time T as: CO = CObias + Kc∙e(t) Where: CObias = controller
-     * bias or null value Kc = controller gain, a tuning parameter e(t) =
-     * controller error = SP – PV (SP = set point PV = measured process
-     * variable)
+    /* The P-Only Algorithm
+     *  ==================== 
+     *  The P-Only controller computes a CO action every loop sample time T as: CO = CObias + Kc∙e(t), Where:
+     *  CObias = controller bias or null value
+     *  Kc = controller gain, a tuning parameter 
+     *  e(t) = controller error = SP – PV (SP = set point, PV = measured process variable)
      */
 
     // if (error > 0)
@@ -46,7 +49,8 @@ public class DriveStraightWithGyroCommand extends Command {
         double process = this.gyro.GetFusedHeading(new FusionStatus()) % 360;
         double error = head - process;
         if (Math.abs(error) > this.TOLERANCE) {
-            leftV += this.P_CONST * error;
+            leftV = this.CONTROLLER_BIAS + this.P_CONST * error;
+            //keeps it in [-12, 12]
             if (leftV > 12)
                 leftV = 12;
             else if (leftV < -12)
@@ -58,8 +62,11 @@ public class DriveStraightWithGyroCommand extends Command {
     @Override
     public void execute() {
         this.pHeading();
-        System.out.println(System.currentTimeMillis() + "Left Error: " + Robot.driveSubSys.getManualErrorLeft());
-        System.out.println(System.currentTimeMillis() + "Right Error: " + Robot.driveSubSys.getManualErrorRight());
+        if (RobotMap.DEBUG) {
+            System.out.println(System.currentTimeMillis() + "Heading: " + Robot.driveSubSys.getImu().GetFusedHeading(new FusionStatus()));
+            System.out.println(System.currentTimeMillis() + "Left Error: " + Robot.driveSubSys.getManualErrorLeft());
+            System.out.println(System.currentTimeMillis() + "Right Error: " + Robot.driveSubSys.getManualErrorRight());
+        }
     }
 
     @Override
@@ -69,7 +76,7 @@ public class DriveStraightWithGyroCommand extends Command {
 
     @Override
     public void interrupted() {
-
+        end();
     }
 
     @Override
