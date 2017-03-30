78fb17a0762289bfff1f607358880a5a2e5dbcd5
Anikait Singh <singh.anikait@gmail.com>
2017-03-30 07:58:42 -0400

    Corrected PController

--- src/org/usfirst/frc/team1923/robot/commands/drive/DriveStraightWithGyroCommand.java
+++ src/org/usfirst/frc/team1923/robot/commands/drive/DriveStraightWithGyroCommand.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team1923.robot.commands.drive;
 
 import org.usfirst.frc.team1923.robot.Robot;
-import org.usfirst.frc.team1923.robot.utils.PIDController;
 
 import com.ctre.CANTalon.TalonControlMode;
 import com.ctre.PigeonImu;
@@ -11,10 +10,8 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveStraightWithGyroCommand extends Command {
 
-    public double leftV, rightV;
-    private PIDController controller;
+    public double leftV = 6, rightV = 6;
     private PigeonImu gyro;
-    private FusionStatus fusionStatus;
     private double head;
 
     private final double P_CONST = 0.070;
@@ -24,11 +21,9 @@ public class DriveStraightWithGyroCommand extends Command {
         this.requires(Robot.driveSubSys);
         Robot.driveSubSys.setDistance(distance);
         this.setTimeout(Math.abs(distance) * 0.05 + 2);
-        leftV = 6;
-        rightV = 6;
         this.gyro = Robot.driveSubSys.getImu();
-        this.fusionStatus = new FusionStatus();
-        this.head = this.gyro.GetFusedHeading(this.fusionStatus) % 360;
+        // initial heading
+        this.head = this.gyro.GetFusedHeading(new FusionStatus()) % 360;
     }
 
     @Override
@@ -36,31 +31,32 @@ public class DriveStraightWithGyroCommand extends Command {
 
     }
 
-    protected double getHeading() {
-        return this.gyro.GetFusedHeading(this.fusionStatus) % 360;
-    }
+    /*
+     * The P-Only Algorithm The P-Only controller computes a CO action every
+     * loop sample time T as: CO = CObias + Kc∙e(t) Where: CObias = controller
+     * bias or null value Kc = controller gain, a tuning parameter e(t) =
+     * controller error = SP – PV SP = set point PV = measured process variable
+     */
 
-    protected double getGyroError() {
-        return head - getHeading();
-    }
-
-    protected void adjustHeading() {
-        if (getGyroError() > 0) {
-            leftV *= (1 + this.P_CONST);
-        } else {
-            leftV *= (this.P_CONST);
+    // if (error > 0)
+    // leftV *= (1 + this.P_CONST);
+    // else leftV *= (this.P_CONST);
+    protected void pHeading() {
+        double process = this.gyro.GetFusedHeading(new FusionStatus()) % 360;
+        double error = head - process;
+        if (Math.abs(error) > this.TOLERANCE) {
+            leftV += this.P_CONST * error;
+            if (leftV > 12)
+                leftV = 12;
+            else if (leftV < -12)
+                leftV = -12;
         }
-        if (leftV > 12)
-            leftV = 12;
-        else if (leftV < -12)
-            leftV = -12;
+        Robot.driveSubSys.drive(leftV, rightV, TalonControlMode.Voltage);
     }
 
     @Override
     public void execute() {
-        if (getGyroError() > this.TOLERANCE)
-            adjustHeading();
-        Robot.driveSubSys.drive(leftV, rightV, TalonControlMode.Voltage);
+        this.pHeading();
         System.out.println(System.currentTimeMillis() + "Left Error: " + Robot.driveSubSys.getManualErrorLeft());
         System.out.println(System.currentTimeMillis() + "Right Error: " + Robot.driveSubSys.getManualErrorRight());
     }
